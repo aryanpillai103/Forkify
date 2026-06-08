b73e4d02fc54f27da7878da3d32ca1ec6bd73eb6
Aryan Pillai <aryanpillai518@gmail.com>
2026-06-08 12:57:32 +0530

    1. fetch recipes from API 2. Render recipes on screen

--- dist/forkify.62406edb.js
+++ dist/forkify.62406edb.js
@@ -1,60 +1,35 @@
-import icons from 'url:../img/icons.svg';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
-
 ///////////////////////////////////////
-const renderSpinner = function(parentEl){
-  const markup = `
-    <div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>
-  `
-  parentEl.innerHTML='';
-  parentEl.insertAdjacentHTML('afterbegin',markup);
-}
-const showRecipe = async function(){
-  //Loading recipe
-  renderSpinner(recipeContainer);
-  try{
-    const res = await fetch(
-      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc990'
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
-    const data = await res.json();
-
-    if(!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res,data);
-    let {recipe} = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients
-    }
-    console.log(recipe);
-
-    //Rendering Recipes
-    const markup = `
+const showRecipe = async function() {
+    try {
+        const res = await fetch(// 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc990'
+        'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res, data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+        //Rendering Recipes
+        const markup = `
     <figure class="recipe__fig">
           <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
           <h1 class="recipe__title">
@@ -65,14 +40,14 @@ const showRecipe = async function(){
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
+              <use href="src/img/icons.svg#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">$recipe.cookingTime</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
+              <use href="src/img/icons.svg#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
@@ -80,12 +55,12 @@ const showRecipe = async function(){
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
+                  <use href="src/img/icons.svg#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
+                  <use href="src/img/icons.svg#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -93,12 +68,12 @@ const showRecipe = async function(){
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="${icons}#icon-user"></use>
+              <use href="src/img/icons.svg#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
+              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
@@ -106,7 +81,7 @@ const showRecipe = async function(){
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${recipe.ingredients.map(ing=>{
+          ${recipe.ingredients.map((ing)=>{
             return `
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
@@ -120,7 +95,7 @@ const showRecipe = async function(){
               </div>
             </li>
             `;
-          }).join('')}
+        }).join('')}
             
 
             <li class="recipe__ingredient">
@@ -155,13 +130,12 @@ const showRecipe = async function(){
           </a>
         </div>
     `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin',markup)
-  }
-  catch(err){
-    alert(err);
-  }
-}
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    } catch (err) {
+        alert(err);
+    }
+};
 showRecipe();
 
-
+//# sourceMappingURL=forkify.62406edb.js.map
